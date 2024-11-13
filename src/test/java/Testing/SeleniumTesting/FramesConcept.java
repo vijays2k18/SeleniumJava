6649f73fdd7375100c5dd732355be6abb78e0f69
vijays2k18 <vijays2k18@gmail.com>
2024-11-13 11:04:15 +0530

    frames

--- src/test/java/Testing/SeleniumTesting/FramesConcept.java
+++ src/test/java/Testing/SeleniumTesting/FramesConcept.java
@@ -1,5 +1,33 @@
 package Testing.SeleniumTesting;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class FramesConcept {
+	
+	@Test
+	public void run()
+	{
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		driver.get("https://jqueryui.com/droppable/");
+		driver.switchTo().frame(driver.findElement(By.className("demo-frame")));
+		Actions action = new Actions(driver);
+		WebElement draggableElement = driver.findElement(By.id("draggable"));
+		WebElement droppableElement = driver.findElement(By.id("droppable"));
+		action.dragAndDrop(draggableElement, droppableElement).build().perform();
+		driver.switchTo().defaultContent();
+	}
 
 }
