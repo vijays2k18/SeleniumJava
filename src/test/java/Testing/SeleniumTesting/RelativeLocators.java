322793097e36ec9eb0bb135b9db819eb0e280703
vijays2k18 <vijays2k18@gmail.com>
2024-11-14 10:37:14 +0530

    updated

---
+++ src/test/java/Testing/SeleniumTesting/RelativeLocators.java
@@ -0,0 +1,26 @@
+package Testing.SeleniumTesting;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import static org.openqa.selenium.support.locators.RelativeLocator.with;
+
+public class RelativeLocators {
+	
+	@Test
+	public void demo() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://rahulshettyacademy.com/angularpractice/");
+		WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
+		System.out.println(driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText());
+		driver.close();
+	}
+}
+
