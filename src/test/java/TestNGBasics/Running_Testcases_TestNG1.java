9a12dd67503ee0c6e64342b92bf0555cb8fde2fd
vijays2k18 <vijays2k18@gmail.com>
2024-11-14 14:26:12 +0530

    testng

---
+++ src/test/java/TestNGBasics/Running_Testcases_TestNG1.java
@@ -0,0 +1,13 @@
+package TestNGBasics;
+
+import org.testng.annotations.Test;
+
+public class Running_Testcases_TestNG1 {
+	
+	@Test                          // @Test annotation is used for running tests in testng
+	public void run()
+	{
+		System.out.println("@Test Annotation is used for Running TestCases in TestNG");
+	}
+
+}
